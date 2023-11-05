b922cf74a6f72a15aef15af7a4f756ee5bfc5dd6
Sakender Saikot <sakendersaikot88@gmail.com>
2023-11-05 23:43:52 +0600

    Ostad_Module_Two

---
+++ bin/first_project.dart
@@ -0,0 +1,23 @@
+void main(){
+
+
+  //Run Time Constant
+  final String userName;
+  userName = 'saikot';
+  print(userName);
+
+
+  final double age;
+  age=20.1;
+  print(age);
+
+  int a = 2;
+  print(a);
+  a++;
+  print(a);
+
+
+
+
+
+}
